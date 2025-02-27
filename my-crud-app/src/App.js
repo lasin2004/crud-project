c3e61d629a9fb5675aee36ddacba7dd076c752af
lasin2004 <lasinbacker@gmail.com>
2025-02-27 21:07:36 +0530

    Update App.js

--- my-crud-app/src/App.js
+++ my-crud-app/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
   return (
     <div className="App">
       <header className="App-header">
@@ -20,7 +20,7 @@ function App() {
       </header>
     </div>
   );
-}
+
 // src/App.js
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
